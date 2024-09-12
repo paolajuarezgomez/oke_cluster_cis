84512b1f9b85e232a95f1d5fc4766e3c6beef7d2
Paola Juárez Gómez <41003502+paolajuarezgomez@users.noreply.github.com>
2024-09-12 14:42:10 +0200

    Delete OKE TF Module/OKE_Cluster directory

--- OKE TF Module/OKE_Cluster/oke/local.tf
+++
@@ -1,7 +0,0 @@
-//noinspection HILUnresolvedReference
-locals {
-  #kubeconfig_content = data.oci_containerengine_cluster_kube_config.kube_config.content
-  #kube_host = yamldecode(local.kubeconfig_content).clusters.0.cluster.server
-  #kube_cluster_ca_certificate = base64decode(yamldecode(local.kubeconfig_content).clusters.0.cluster.certificate-authority-data)
-  oke_x86_image_id = one(toset([for image in data.oci_containerengine_node_pool_option.oke_images.sources : image.image_id if image.source_name == var.oke_image_name]))
-}
